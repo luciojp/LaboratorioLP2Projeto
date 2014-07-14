deffe79a507c0734aa6115ea41e0a0f19b5f187e
C. Filemon <clenimar.filemon@gmail.com>
2014-07-14 20:47:55 -0300

    Construtor de Usuário já o adiciona ao BD
    Afinal, como vamos checar e-mail válido se os usuários novos que criamos
    não são adicionados ao BD?
    
    Agora, se o construtor de Usuário rodar sem lançar exceções, ele já é
    adicionado ao Array dentro de Banco de Dados. Com isso, não é necessário
    fazer essa operação em Gerente.

--- src/ufcg/cc/lp2/logica/Usuario.java
+++ src/ufcg/cc/lp2/logica/Usuario.java
@@ -1,10 +1,13 @@
 package ufcg.cc.lp2.logica;
 
 public class Usuario extends Pessoa{
+	
+	private BancoDeDados bd = new BancoDeDados();
 
 	public Usuario(String nome, String email, String senha1, String senha2,
 			String dicaSenha) throws Exception {
 		super(nome, email, senha1, senha2, dicaSenha);
+		bd.adicionarUsuario(this);
 	}
 
 }
